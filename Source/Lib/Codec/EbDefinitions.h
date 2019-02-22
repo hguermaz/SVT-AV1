4094f98ffe2d696cec7ce77812e183fce1cdfd58
hguermaz <hassen.guermazi@intel.com>
2019-02-21 17:31:35 -0800

    set reference: all mdc

--- Source/Lib/Codec/EbDefinitions.h
+++ Source/Lib/Codec/EbDefinitions.h
@@ -154,7 +154,7 @@ extern "C" {
 
 
 
-#define OPEN_LOOP_EARLY_PARTITION                        0
+#define OPEN_LOOP_EARLY_PARTITION                        1
 #define ADAPTIVE_DEPTH_PARTITIONING                      0 // Added the ability to switch @ SB basis between: (1) all square up to 64x64,  (2) mdc up to 64x64, (3) mdc up to 64x64 only pred, (4) mdc up to 64x64 only pred + 1 NFL
 #if ADAPTIVE_DEPTH_PARTITIONING
 #define ADP_STATS_PER_LAYER                              0
