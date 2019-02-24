5e02683acf31a32cb859d7a8f25401c16d637b6a
arthurkkhi <44004438+arthurkkhi@users.noreply.github.com>
2019-02-23 21:17:42 -0800

    M3_20190223_9pm

--- Source/Lib/Codec/EbPictureDecisionProcess.c
+++ Source/Lib/Codec/EbPictureDecisionProcess.c
@@ -646,12 +646,7 @@ EbErrorType signal_derivation_multi_processes_oq(
         picture_control_set_ptr->pic_depth_mode = PIC_ALL_C_DEPTH_MODE;
     }
     else if (picture_control_set_ptr->enc_mode <= ENC_M3) {
-        if (picture_control_set_ptr->slice_type != I_SLICE) {
-            picture_control_set_ptr->pic_depth_mode = PIC_SQ_DEPTH_MODE;
-        }
-        else {
-            picture_control_set_ptr->pic_depth_mode = PIC_ALL_DEPTH_MODE;
-        }
+        picture_control_set_ptr->pic_depth_mode = PIC_SQ_NON4_DEPTH_MODE;
     }
 #if ADAPTIVE_DEPTH_PARTITIONING
     else if (picture_control_set_ptr->enc_mode == ENC_M3) {
