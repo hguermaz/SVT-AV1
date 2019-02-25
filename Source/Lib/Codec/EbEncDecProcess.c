21e6c34c22517dab42e34b283d279f08d2831f76
Kao, Arthur <arthur.kao@intel.com>
2019-02-25 14:26:23 -0800

    + new M4 settings

--- Source/Lib/Codec/EbEncDecProcess.c
+++ Source/Lib/Codec/EbEncDecProcess.c
@@ -1353,8 +1353,10 @@ EbErrorType signal_derivation_enc_dec_kernel_oq(
         context_ptr->nfl_level = 0;
     else if (picture_control_set_ptr->enc_mode <= ENC_M2)
         context_ptr->nfl_level = 1;
-    else if (picture_control_set_ptr->enc_mode == ENC_M3)
+    else if (picture_control_set_ptr->enc_mode <= ENC_M3)
         context_ptr->nfl_level = 2;
+    else if (picture_control_set_ptr->enc_mode <= ENC_M4)
+        context_ptr->nfl_level = 3;
     else
         context_ptr->nfl_level = 3;
 
