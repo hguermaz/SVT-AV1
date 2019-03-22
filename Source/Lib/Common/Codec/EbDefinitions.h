9589d8716c0966206ea14b434d24bfcd05013ac5
adixonn <adam.dixon@intel.com>
2019-03-22 10:48:00 -0700

    Removed OPEN_LOOP_EARLY_PARTITION definition

--- Source/Lib/Common/Codec/EbDefinitions.h
+++ Source/Lib/Common/Codec/EbDefinitions.h
@@ -71,12 +71,9 @@ extern "C" {
 #define TEST5_DISABLE_NSQ_ME                            0
 
 
-#define OPEN_LOOP_EARLY_PARTITION                       1
-#if OPEN_LOOP_EARLY_PARTITION
 #define REST_FAST_RATE_EST                              1
 #define MDC_FIX_0                                       1
 #define MDC_FIX_1                                       1
-#endif
 
 #define M8_ADP                                          1
 #if M8_ADP
