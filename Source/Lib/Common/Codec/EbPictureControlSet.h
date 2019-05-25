45702fcfc3e49ee3fd142e53fff1db82b02c9ca6
hguermaz <hassen.guermazi@intel.com>
2019-05-24 17:52:23 -0700

    clean up

--- Source/Lib/Common/Codec/EbPictureControlSet.h
+++ Source/Lib/Common/Codec/EbPictureControlSet.h
@@ -14234,6 +14234,10 @@ extern "C" {
 #if M8_SKIP_BLK
         uint8_t                               skip_sub_blks;
 #endif
+#if ATB_SUPPORT
+        uint8_t                               atb_level;
+#endif
+
         //**********************************************************************************************************//
         FrameType                            av1_frame_type;
         Av1RpsNode                          av1_ref_signal;
